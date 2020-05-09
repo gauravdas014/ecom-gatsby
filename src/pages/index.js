6f88f2887115ea9252288f630918b2c7bb5bde20
Gaurav Das <gauravdas014@gmail.com>
2020-05-09 12:38:30 +0530

    hero section done, graph ql used which displays image as svg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroSection from "../components/Reusable/HeroSection"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="I write Code"
+      subtitle="LearnCodeOnline.in"
+      heroclass="hero-background"
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
