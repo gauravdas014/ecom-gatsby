b8e984fc39ba14ced5b75d17a14793685e4208fd
Gaurav Das <gauravdas014@gmail.com>
2020-05-09 15:46:02 +0530

    our services page done

---
+++ src/components/Services/Services.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+export default function Services() {
+  return (
+    <div>
+      <div className="row row-services">
+        <div className="col-3 max-auto col-services">
+          <h1>Service One</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit at
+            quasi pariatur quisquam, debitis explicabo sapiente, nemo obcaecati
+            aspernatur, cum facere cupiditate nam quidem. Voluptatibus explicabo
+            nostrum eos maiores consequatur.
+          </p>
+        </div>
+        <div className="col-3 max-auto col-services">
+          <h1>Service Two</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit at
+            quasi pariatur quisquam, debitis explicabo sapiente, nemo obcaecati
+            aspernatur, cum facere cupiditate nam quidem. Voluptatibus explicabo
+            nostrum eos maiores consequatur.
+          </p>
+        </div>
+        <div className="col-3 max-auto col-services">
+          <h1>Service Three</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit at
+            quasi pariatur quisquam, debitis explicabo sapiente, nemo obcaecati
+            aspernatur, cum facere cupiditate nam quidem. Voluptatibus explicabo
+            nostrum eos maiores consequatur.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/services.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/Reusable/HeroSection"
+import Infoblock from "../components/Reusable/Infoblock"
+import DualInfoBlock from "../components/Reusable/DualInfoBlock"
+import Services from "../components/Services/Services"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="Our Services"
+      subtitle=""
+      heroclass="services-background"
+    />
+    <Infoblock heading="About Us" />
+    <DualInfoBlock heading="Message from CTO" />
+    <Services />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
