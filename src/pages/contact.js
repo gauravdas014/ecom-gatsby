1d8dfa561a232906cd61a397de6774fc7e900b7b
Gaurav Das <gauravdas014@gmail.com>
2020-05-09 20:20:18 +0530

    dummy contact us page for deployment

---
+++ src/pages/contact.js
@@ -0,0 +1,57 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/Reusable/HeroSection"
+import Infoblock from "../components/Reusable/Infoblock"
+import DualInfoBlock from "../components/Reusable/DualInfoBlock"
+import Coursecart from "../components/Cart/Coursecart"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="I write Code"
+      subtitle="LearnCodeOnline.in"
+      heroclass="hero-background"
+    />
+    <Infoblock heading="About Us" />
+    <Coursecart courses={data.courses} />
+    <DualInfoBlock heading="Our team" />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    courses: allContentfulCourses {
+      edges {
+        node {
+          id
+          title
+          price
+          category
+          description {
+            description
+          }
+          image {
+            fixed(width: 200, height: 120) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
