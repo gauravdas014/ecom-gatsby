ea19521bb354827f0af52adc28d396f4bd1db3f7
Gaurav Das <gauravdas014@gmail.com>
2020-05-09 10:17:33 +0530

    navbar basic done

---
+++ src/components/Reusable/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import logo from "../../images/logo.png"
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-theme text-white">
+        <Link to="/" className="navbar-brand ml-5">
+          <img src={logo} alt="logo goes here" width="40px" />
+        </Link>
+        <button className="navbar-toggler" type="button">
+          <span className="text-white">menu</span>
+        </button>
+      </nav>
+    )
+  }
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,14 +8,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Footer from "./Reusable/Footer"
+import Navbar from "./Reusable/Navbar"
 
 import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => (
   <>
+    <Navbar />
     {children}
     <Footer />
   </>
