d6c4a3c489fd919c40ec882333663897649c63c7
Gaurav Das <gauravdas014@gmail.com>
2020-05-09 09:17:39 +0530

    footer done

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,26 +9,17 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Footer from "./Reusable/Footer"
+
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <div>
-      <main>{children}</main>
-    </div>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    {children}
+    <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
