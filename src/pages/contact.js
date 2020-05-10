1c5e80564e746cc73ac85b2ffbc8e44c77c1e738
Gaurav Das <gauravdas014@gmail.com>
2020-05-10 05:57:17 +0530

    contact page basic layout done

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,53 +5,31 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/Reusable/HeroSection"
 import InfoBlock from "../components/Reusable/InfoBlock"
-import DualInfoBlock from "../components/Reusable/DualInfoBlock"
-import Coursecart from "../components/Cart/Coursecart"
 
-const IndexPage = ({ data }) => (
+const ContactPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
 
     <HeroSection
       img={data.img.childImageSharp.fluid}
-      title="I write Code"
-      subtitle="LearnCodeOnline.in"
-      heroclass="hero-background"
+      title="Contact Us"
+      subtitle=""
+      heroclass="about-background"
     />
-    <InfoBlock heading="About Us" />
-    <Coursecart courses={data.courses} />
-    <DualInfoBlock heading="Our team" />
+    <InfoBlock heading="Hw can we help?" />
   </Layout>
 )
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "heromain.png" }) {
+    img: file(relativePath: { eq: "contact.png" }) {
       childImageSharp {
         fluid(maxWidth: 400, maxHeight: 250) {
           ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
-    courses: allContentfulCourses {
-      edges {
-        node {
-          id
-          title
-          price
-          category
-          description {
-            description
-          }
-          image {
-            fixed(width: 200, height: 120) {
-              ...GatsbyContentfulFixed_tracedSVG
-            }
-          }
-        }
-      }
-    }
   }
 `
 
-export default IndexPage
+export default ContactPage
